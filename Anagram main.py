10312d20e66abbb21fa998a81a33390f7dbe78e7
Richard Akinwande <105463522+Wandeboi@users.noreply.github.com>
2022-05-28 04:34:00 +0100

    Add files via upload

---
+++ Anagram main.py
@@ -0,0 +1,27 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+s1 ='listen'
+s2 ='silent'
+
+if(sorted(s1) == sorted(s2)):
+        print ('True, the words are Anagram')
+else:
+        print('False, the words are not Anagram')
+
+        
+
+s1 ='hello'
+s2 ='check'
+
+if(sorted(s1) == sorted(s2)):
+        print ('True, the words are Anagram')
+else:
+        print('False, the words are not Anagram')
+
+        
+        
+
