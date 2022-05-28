e61ef3636b9e37ee130a35e57a88e0aa51f87d12
Richard Akinwande <105463522+Wandeboi@users.noreply.github.com>
2022-05-28 05:40:28 +0100

    Add files via upload

---
+++ class main.py
@@ -0,0 +1,29 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self):
+        pass
+
+
+
+
+class Student:
+    
+
+        name = "Richard"
+        age = "23years"
+        track = "Full stack"
+        score = "98"
+
+       
+        def fun(self):
+            print("I'm", self.name)
+            print("I'm", self.age)
+            print("I'm enroll to", self.track)
+            print("My score is", self.score)
+
+
+Richard = Student()
+print(Richard.name)
+Richard.fun()
+
+        
